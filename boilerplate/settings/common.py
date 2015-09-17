74b12bd1be458c0bbd3bd657f78c4e5be1d51e7a
ratnim <martin.fischer@student.hpi.uni-potsdam.de>
2015-09-17 09:34:04 +0200

    fix stay loggin problem

--- boilerplate/settings/common.py
+++ boilerplate/settings/common.py
@@ -101,6 +101,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
